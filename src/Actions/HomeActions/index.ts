adae8ac7a452e370da157b281afed5a092044b55
jinhojang6 <jinho5051@gmail.com>
2020-06-09 02:12:20 +0900

    Add old Embark tutrials

--- src/Actions/HomeActions/index.ts
+++ src/Actions/HomeActions/index.ts
@@ -18,6 +18,7 @@ var parseString = require('xml2js').parseString;
 const CORS_PROXY = "https://cors-fix.status.im/";
 
 const embarkBlog = "https://blog.embarklabs.io/atom.xml";
+const embarkOldBlog = "https://framework.embarklabs.io/atom.xml";
 
 const nimbusBlog = "https://our.status.im/ghost/api/v2/content/posts/?key=10e7f8c1f793d2945ea1177076&filter=tag:nim";
 
@@ -60,7 +61,34 @@ export const FetchEmbark = async () => {
                     parsedEmbarkData.push(postData)
                 }
             })
-            console.log(parsedEmbarkData)
+        });
+    })
+    .then(async () => {
+        await FetchOldEmbark();
+    });
+}
+
+export const FetchOldEmbark = async () => {
+    await fetch(`${CORS_PROXY}`+ `${embarkOldBlog}`)
+    .then(response => response.text())
+    .then(data => {
+        embarkData = data
+        parseString(embarkData, function (err:any, result:any) {
+            const entries: Array<any> = result.feed.entry;
+            entries.forEach(entry => {
+                const category = entry.category[0]['$']['term']
+                const title = entry.title[0];
+                const checkDuplicate = title == "How to upgrade to Embark 4" || title == "Introduction to Web3 - What Are Your Options?" || 
+                    title.includes("Nim vs Crystal")
+                if (category === 'tutorials' && !checkDuplicate) {
+                    const postData: any = {}
+                    postData.title = entry.title[0];
+                    postData.published = entry.published[0];
+                    postData.url = entry.id[0];
+                    postData.summary = entry.summary[0]._;
+                    parsedEmbarkData.push(postData)
+                }
+            })
         });
     });
 }
