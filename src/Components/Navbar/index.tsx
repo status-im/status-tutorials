a04d1530efd9bc19470722083eed8fcfc2eba5a9
jinhojang6 <jinho5051@gmail.com>
2020-06-10 01:55:14 +0900

    Update the nav bar

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -22,13 +22,22 @@ const Navbar: React.FunctionComponent<INavbar.IProps> = (): JSX.Element => {
                 </div >
                 <Menu right disableAutoFocus pageWrapId={ "nav" } outerContainerId={ "main" } isOpen={ isOpen } >
                     <a href="https://statusnetwork.com/" target="_blank" rel="noopener noreferrer" onClick={closeNav}>
-                        About us
+                        Status Network
+                    </a>                    
+                    <a href="https://status.im/docs/" target="_blank" rel="noopener noreferrer" onClick={closeNav}>
+                        Status Docs
                     </a>
-                    <a href="https://github.com/status-im" target="_blank" rel="noopener noreferrer" onClick={closeNav}>
-                        Github
+                    <a href="https://keycard.tech/docs/" target="_blank" rel="noopener noreferrer" onClick={closeNav}>
+                        Keycard Docs
                     </a>
-                    <a href="https://our.status.im/" target="_blank" rel="noopener noreferrer" onClick={closeNav}>
-                        Blog
+                    <a href="https://framework.embarklabs.io/docs/" target="_blank" rel="noopener noreferrer" onClick={closeNav}>
+                        Embark Docs
+                    </a>
+                    <a href="https://nimbus.team/docs/" target="_blank" rel="noopener noreferrer" onClick={closeNav}>
+                        Nimbus Docs
+                    </a>
+                    <a href="https://vac.dev/research-log/" target="_blank" rel="noopener noreferrer" onClick={closeNav}>
+                        Vac Research Log
                     </a>
                 </Menu>
             </nav>
