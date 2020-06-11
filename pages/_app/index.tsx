c9a7dd2354f569e2ec2e9ae27355779ab0124e49
jinhojang6 <jinho5051@gmail.com>
2020-06-12 01:34:21 +0900

    Update og & twitter descriptions

--- pages/_app/index.tsx
+++ pages/_app/index.tsx
@@ -41,9 +41,9 @@ class WebApp extends App<AppWithStore> {
                     <meta name="theme-color" content="#000000"/>
                     <meta property="og:title" content="The Status Network Tutorials" />
                     <meta property="og:image" content="https://github.com/status-im/ambassador.status.im/blob/develop/public/status-logo-symbol.jpg?raw=true" />
-                    <meta property="og:description" content="The Status Network Tutorials" />
+                    <meta property="og:description" content="Welcome to the Status Network tutorials archive" />
                     <meta name="twitter:title" content="The Status Network Tutorials" />
-                    <meta name="twitter:description" content="The Status Network Tutorials" />
+                    <meta name="twitter:description" content="Welcome to the Status Network tutorials archive" />
                     <meta name="twitter:image" content="https://github.com/status-im/ambassador.status.im/blob/develop/public/status-logo-symbol.jpg?raw=true" />
                     <meta name="twitter:card" content="summary" />
                     <meta name="twitter:site" content="@ethstatus" />     
