5d0d9813cf71815ca9fb88c822b81bfb2beb689c
jinhojang6 <jinho5051@gmail.com>
2020-06-18 02:28:21 +0900

    Add tutorials for Status

--- src/Components/Blogs/Status.tsx
+++ src/Components/Blogs/Status.tsx
@@ -15,6 +15,7 @@ const Status = () => {
                                     <div className="post">
                                         <div className="meta"><a href="https://status.im/">Status</a></div>
                                         <h4 ><a className="post-title" href="https://status.im/technical/build_status/status_react_quickstart.html">Status-React Quickstart Guide</a></h4>
+                                        <div className="author" style={{ paddingRight: '10px' }}>By <div className="author-name">robin_percy</div></div>
                                         <div className="description" style={{ paddingRight: '10px' }}>Build Status-react on your machine.</div>
                                     </div>                         
                                 </li>
@@ -23,6 +24,14 @@ const Status = () => {
                                         <div className="meta"><a href="https://status.im/">Status</a></div>
                                         <h4 ><a className="post-title" href="https://status.im/technical/build_status/status_go.html">Build Status-Go</a></h4>
                                         <div className="description" style={{ paddingRight: '10px' }}>Status-go is an underlying part of Status depending on go-ethereum which is forked and modified by us.</div>
+                                    </div>                         
+                                </li>
+                                <li key="3">
+                                    <div className="post">
+                                        <div className="meta"><a href="https://status.im/">Status</a></div>
+                                        <h4 ><a className="post-title" href="https://our.status.im/reproducable-builds-with-nix/">Reproducible Builds with Nix</a></h4>
+                                        <div className="author" style={{ paddingRight: '10px' }}>By <div className="author-name">Jakub Sokołowski</div></div>
+                                        <div className="description" style={{ paddingRight: '10px' }}>Nix is a tool which uses a subset of Haskel programming language to define the entire tree of software dependencies necessary to manage an entire Linux operating system: NixOS.</div>
                                     </div>                         
                                 </li>
                             </ul>
