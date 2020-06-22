dc101f5db73c5988d12fb9c20af526592ca3360e
jinhojang6 <jinho5051@gmail.com>
2020-06-23 00:54:14 +0900

    Add font awesome to _app

--- pages/_app/index.tsx
+++ pages/_app/index.tsx
@@ -12,6 +12,10 @@ import { appWithTranslation } from "@Server/i18n";
 import { AppWithStore } from "@Interfaces";
 import { makeStore } from "@Redux";
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 import "@Static/css/main.scss";
 // #endregion Local Imports
 
